d5a7f4cac46d7dbd74d21ef0c7d6d5f7e52a6c51
JOSUÉ ELÍAS GARCÍA LÓPEZ <USSS046123@ugb.edu.sv>
2024-09-04 09:44:17 -0600

    suma de dos numeros introducidos por el usuario

---
+++ miPrimerProyecto/miPrimerProyecto/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Num1: ");
+int Num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Num2: ");
+int Num2 = int.Parse(Console.ReadLine());
+
+int respuesta = Num1 + Num2;
+Console.WriteLine("La suma de  {0} + {1} = {2}", Num1, Num2, respuesta);
+
+//Pausa.
+
+Console.ReadLine();
